7de643be7ec1c414e39ae9814f8b1ec6e6a5722e
Jajo <donjajo4all@gmail.com>
2018-12-21 11:28:37 +0100

    Built front page Included icon fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Col, Row } from 'react-bootstrap'
+import './css/bootstrap.min.css'
+import './css/index.css'
+import tux from './images/tux.jpg'
 
 class App extends React.Component {
+    constructor( props ) {
+        super( props )
+
+        this.state = {
+            window_height : window.innerHeight 
+        }
+    }
+
     render() {
-        return <div>Hello World</div>
+        return( 
+            <div>
+                <div className="mg-top-150">
+                    <div className="text-center">
+                        <img src={ tux } className="tux" style={ { width : '300px' } } />
+                    </div>
+                    <div style={ { position : 'relative', height : this.state.window_height } }>
+                        <div className="grad">
+                            <div className="text-center">
+                                <h1 className="text-center">Linux Nigeria</h1>
+                                <h4>Official page is coming up soon! <br /> <br /> <small>Good with React js? CSS? PHP? Want to help? This site is on <a href="https://github.com/donjajo/linux.ng" target="_blank">GitHub</a></small> </h4>
+
+                                <span>Mail us: lagos [at] linux.ng</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
+
+render( <App />, document.getElementById( 'root' ) )
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,32 @@ module.exports = {
                     babelrc: false,
                     presets : [ '@babel/env', '@babel/react' ]
                 }
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options : {
+                    outputPath : function( url, resourcePath, context ) {
+                        if( /\.(png|jpg|gif)/.test( resourcePath ) ) 
+                            return '../images/' + url
+                        else if( /\.(svg|eot|ttf|woff|woff2)$/.test( resourcePath ) ) 
+                            return '../fonts/' + url
+                        else 
+                            return './'
+                    },
+                    publicPath : function( url, resourcePath, context ) {
+                        if( /\.(png|jpg|gif)/.test( resourcePath ) ) 
+                            return './assets/images/' + url
+                        else if( /\.(svg|eot|ttf|woff|woff2)/.test( resourcePath ) ) 
+                            return './assets/fonts/' + url
+                        else
+                            return './assets'
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
